53fff72cd8f8e8c2d912c73a7e040ee5cd844a83
nickatkins04 <naatkins42@tntech.edu>
2024-11-28 12:59:30 -0600

    working on Pokemon.h

--- Pokemon.h
+++ Pokemon.h
@@ -6,6 +6,34 @@ class Pokemon
 	private:
 		string name;
 		int idNum;
+		string type;
+		string ability;
+
+	public:
+		//constructor/destructor
+		Pokemon();
+
+		~Pokemon();
+
+		//setters
+		void setName(string name);
+
+		void setIdNum(int idNum);
+
+		void setType(string type);
+
+		void setAbility(string ability);
+
+		//getters
+		string getName();
+
+		int getIdNum();
+
+		string getType();
+
+		string getAbility();
+
+		//overloaded operators
 };
 
 #endif
