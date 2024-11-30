fa9c6c7affad33925e3bdc7a716723ecad838bcc
nickatkins04 <naatkins42@tntech.edu>
2024-11-29 20:18:01 -0600

    editing intro dialog

--- Driver.cpp
+++ Driver.cpp
@@ -6,50 +6,42 @@ using namespace std;
 
 int main()
 {
-    int input1, input2, selection, pokemonIDNum; 
+    int selection, pokemonIDNum;
     string pokemonName, pokemonType, pokemonAbility;
 
     Pokemon obj;
-	Tree pokedex;
-
-    cout << "\n\nWelcome to the beautiful region of Kanto!\n";
-    cout << "As a young Pokemon trainer, you're going to want this.\n\n";
-    cout << "Press anything, then enter.\n";
-
-    cin >> input1;
+    Tree pokedex;
 
     cout << "\n\nYou obtained a POKEDEX!\n\n";
 
     cout << "This will give you a place to log the Pokemon you cross paths with.\n\n";
 
-    cout << "Now, off on your way you go!\n";
+    cout << "Now, off on your way you go!\n\n";
     cout << "Be safe, and have a wonderful time getting to know the creatures this region has to offer!\n\n\n";
-
-    cout << "Press anything, then enter.\n";
-
-    cin >> input2;
 
     cout << "\n\nWelcome to your Pokedex!\n\n";
     cout << "Here, you can enter the name, number index (ID Number), type, and ability of the Pokemon you just caught.\n";
 
-	do{
-		cout << "What would you like to do?\n\n";
-		cout << "1. Add a Pokemon.\n";
-		cout << "2. Delete a Pokemon.\n";
-		cout << "3. Display pokemon entry.\n";
-		cout << "4. Display entire list of Pokemon.\n\n";
-		cout << "5. Close Pokedex.\n";
+    do
+    {
+        cout << "What would you like to do?\n\n";
+        cout << "1. Add a Pokemon.\n";
+        cout << "2. Delete a Pokemon.\n";
+        cout << "3. Display pokemon entry.\n";
+        cout << "4. Display entire list of Pokemon.\n\n";
+        cout << "5. Close Pokedex.\n";
 
-		cin >> selection;
+        cin >> selection;
 
-		cout << endl << endl;
-		
-		switch (selection)
-		{
-			case 1:
-				cout << "Pokemon's NAME: ";
-				cin >> pokemonName;
-				obj.setName(pokemonName);
+        cout << endl
+             << endl;
+
+        switch (selection)
+        {
+        case 1:
+            cout << "Pokemon's NAME: ";
+            cin >> pokemonName;
+            obj.setName(pokemonName);
 
             cout << "\nPokemon's ID NUMBER: ";
             cin >> pokemonIDNum;
@@ -63,28 +55,28 @@ int main()
             cin >> pokemonAbility;
             obj.setAbility(pokemonAbility);
 
-				pokedex.insertNode(obj);
-			
-			case 2:
-				cout << "Enter ID NUMBER of the Pokemon you would like to delete:";
-				cin >> pokemonIDNum;
-				pokedex.remove(pokemonIDNum);
+            pokedex.insertNode(obj);
 
-			case 3:
-				cout << "Enter ID NUMBER of the Pokemon entry you would like to see:";
-				cin >> pokemonIDNum;
-				pokedex.printNode(pokemonIDNum);
+        case 2:
+            cout << "Enter ID NUMBER of the Pokemon you would like to delete:";
+            cin >> pokemonIDNum;
+            pokedex.remove(pokemonIDNum);
 
-			case 4:
-				pokedex.displayInOrder();
-			
-			case 5:
-				break;
+        case 3:
+            cout << "Enter ID NUMBER of the Pokemon entry you would like to see:";
+            cin >> pokemonIDNum;
+            pokedex.printNode(pokemonIDNum);
 
-			default:
-				cout << "This is not an option.\n";
-		}
-	}while(selection != 5);
+        case 4:
+            pokedex.displayInOrder();
 
-	return 0;
+        case 5:
+            break;
+
+        default:
+            cout << "This is not an option.\n";
+        }
+    } while (selection != 5);
+
+    return 0;
 }
